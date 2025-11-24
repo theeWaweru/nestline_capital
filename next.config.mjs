27963221b403cecaef34224cb2504e66a5f11cd9
davidngari47@gmail.com <davidngari47@gmail.com>
2025-11-24 23:29:34 +0300

    next config settings update - nestline

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     globalNotFound: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nestline-capital-assets.s3.af-south-1.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.af-south-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
